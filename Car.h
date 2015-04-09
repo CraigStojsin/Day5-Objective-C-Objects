70b7720973179aed337c2e3a7364834298f1bf02
CraigStojsin <stojsin@outlook.com>
2015-04-09 13:43:24 -0400

    created a car class and a property for it

---
+++ Car.h
@@ -0,0 +1,26 @@
+//
+//  Car.h
+//  Day5-Objects
+//
+//  Created by Craig Stojsin on 2015-04-09.
+//  Copyright (c) 2015 Codes By Craig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject{
+    // protected instance variables ( not recommended)
+    
+}
+
+@property (copy) NSString* model;
+
+
+
+-(void)drive;
+
+
+
+
+
+@end
---
+++ Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  Day5-Objects
+//
+//  Created by Craig Stojsin on 2015-04-09.
+//  Copyright (c) 2015 Codes By Craig. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
