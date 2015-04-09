e0a7788d5197454999bc63d27a3bceb5de50f6d2
CraigStojsin <stojsin@outlook.com>
2015-04-09 17:02:58 -0400

    5

--- Day5_Challenge1/Car.h
+++ Day5_Challenge1/Car.h
@@ -15,10 +15,16 @@
 -(void) fillUpGas;
 -(void) changeTire;
 
+
+//Question 3
+
 // a motorcycle overlaps with every action
 // a boat however has no tires and shouldnt inherit that action
 
+//Question 4
 //the advantages of being able to apply and action to an object that is from one of the classes means you dont have to write a new object each time because it can inherit the similar traits such as drive, and change tire if it applies because the code is already there for you.
 
+//Question 5
+//The OOP approach makes a lot more sense because we can use the inherit values instead of writing code for each vehicle, we only have to write it once and then apply it to each different vehicle
 
 @end
